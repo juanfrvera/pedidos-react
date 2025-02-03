259035098a61afa61c87d8f2a44a3632a3c5bd97
Juan Vera <juanfrvera@gmail.com>
2025-02-03 09:14:52 -0300

    feat: home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,21 @@
 "use client";
-import styles from "./page.module.scss";
 
 export default function Home() {
-  return <div>Welcome what do you want to order today?</div>;
+  return (
+    <div>
+      <div className="header">
+        <div className="title">Pedidos</div>
+        <div className="login">Log In</div>
+      </div>
+      <div className="body">
+        <div>Welcome what do you want to order today?</div>
+        <div className="shop-creation">
+          <div>Want to create your shop?</div>
+          <button className="shop-creation__create button button-primary">
+            Create
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
